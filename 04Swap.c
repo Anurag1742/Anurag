cfb25bc0869216d563788762474f7606050a0910
Anurag1742 <anuragranjan373@gmail.com>
2025-01-30 13:47:15 +0530

    Create 04Swap.c

---
+++ 04Swap.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+double harmonicSum(int n) {
+    double sum = 0.0;
+
+    for (int i = 1; i <= n; i++) {
+        sum += 1.0 / i;
+    }
+
+    return sum;
+}
+
+int main() {
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+
+       double result = harmonicSum(n);
+    printf("The sum of the harmonic series for n = %d is: %.6f\n", n, result);
+
+    return 0;
+}
