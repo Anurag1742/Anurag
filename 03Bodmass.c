f91344dde29e23c051e22a7be82c90a6589968c9
Anurag1742 <anuragranjan373@gmail.com>
2025-01-30 13:26:04 +0530

    Create 03Bodmass.c

---
+++ 03Bodmass.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f,g;
+    printf("Enter 1st num :- ");
+    scanf("%d",&a);
+    printf("Enter 2nd num :- ");
+    scanf("%d",&b);
+
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    g=a%b;
+    
+    printf("Sum :- %d\nSubtract :- %d\nMultiplication :- %d\nProduct :- %d\nQuotient :- %d",c,d,e,f,g);
+    return 0;
+}
